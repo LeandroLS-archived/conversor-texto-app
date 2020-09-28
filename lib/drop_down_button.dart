fe8a0c40a235f53cbb7e82327652092df10c15f9
Leandro Lima da Silva <leandro.lsilva@b2wdigital.com>
2020-09-28 15:48:02 -0300

    Dando forma ao app

---
+++ lib/drop_down_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+
+class DropDownButton extends StatefulWidget {
+  @override
+  _DropDownButtonState createState() => _DropDownButtonState();
+}
+
+class _DropDownButtonState extends State<DropDownButton> {
+  
+String dropdownValue = 'Selecione';
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      onChanged: (String newValue) {
+      
+      },
+      items: <String>['Selecione', 'Reverso', 'MAÍUSCULAS', 'minúsculas', 'aLtErNaDo']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
