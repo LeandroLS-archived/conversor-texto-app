eeab37d478be6fd58809cae6c6ed3cbefbd167e4
Leandro Lima da Silva <leandro.lsilva@b2wdigital.com>
2020-09-29 11:54:28 -0300

    finalizando app

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,40 @@ class _State extends State<MyApp> {
   final _formKey = GlobalKey<FormState>();
   String _dropdownValue = 'Selecione';
   String _userText;
-  String _textConverted;
+  static const List<String> opcoes = [
+    'Selecione',
+    'Reverso',
+    'MAÍUSCULAS',
+    'minúsculas',
+  ];
+  final _myController = TextEditingController();
+
+  textToUpper(String str) {
+    return str.toUpperCase();
+  }
+
+  textToLower(String str) {
+    return str.toLowerCase();
+  }
+
+  textReversed(String str) {
+    return str.split('').reversed.join();
+  }
+
+  convertText(String str) {
+    switch (_dropdownValue) {
+      case 'Reverso':
+        return textReversed(str);
+        break;
+      case 'MAÍUSCULAS':
+        return textToUpper(str);
+        break;
+      case 'minúsculas':
+        return textToLower(str);
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -49,41 +82,36 @@ class _State extends State<MyApp> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: DropdownButton<String>(
-                          value: _dropdownValue,
-                          icon: Icon(Icons.arrow_downward),
-                          iconSize: 24,
-                          elevation: 16,
-                          onChanged: (String newValue) {
-                            setState(() {
-                              _dropdownValue = newValue;
-                            });
-                          },
-                          items: <String>[
-                            'Selecione',
-                            'Reverso',
-                            'MAÍUSCULAS',
-                            'minúsculas',
-                            'aLtErNaDo'
-                          ].map<DropdownMenuItem<String>>((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
-                        )),
+                    Container(
+                      width: 135,
+                      child: DropdownButtonFormField<String>(
+                        validator: (value) => value == 'Selecione' ? 'Selecione um valor.' : null,
+                        value: _dropdownValue,
+                        icon: Icon(Icons.arrow_downward),
+                        iconSize: 24,
+                        elevation: 16,
+                        onChanged: (String newValue) {
+                          setState(() {
+                            _dropdownValue = newValue;
+                          });
+                        },
+                        items: opcoes
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
+                    ),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: RaisedButton(
                         onPressed: () {
                           if (_formKey.currentState.validate()) {
-                            print('Validado');
-                            print('_dropdownValue: $_dropdownValue');
-                            print('_userText: $_userText');
-
-
+                            setState(() {
+                              _myController.text = convertText(_userText);
+                            });
                           }
                         },
                         child: const Text(
@@ -94,12 +122,13 @@ class _State extends State<MyApp> {
                   ],
                 ),
                 TextFormField(
+                  maxLines: 5,
+                  controller: _myController,
                   decoration: InputDecoration(
                     border: const OutlineInputBorder(),
                     filled: true,
                     labelText: 'Resultado',
                   ),
-                  maxLines: 5,
                 ),
               ],
             ),
