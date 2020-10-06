477b5fd833137b0f9fd52b347c46a70180316b36
Leandro Lima da Silva <leandro.lsilva@b2wdigital.com>
2020-10-06 15:39:23 -0300

    tentando melhorar perfomance

---
+++ lib/widgets/text_areas.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+class TextAreas extends StatefulWidget {
+  @override
+  _TextAreasState createState() => _TextAreasState();
+}
+
+class _TextAreasState extends State<TextAreas> {
+  final _formKey = GlobalKey<FormState>();
+  String _dropdownValue = 'Selecione';
+  String _userText;
+  static const List<String> opcoes = [
+    'Selecione',
+    'Reverso',
+    'MAÍUSCULAS',
+    'minúsculas',
+  ];
+  final _myController = TextEditingController();
+
+  textToUpper(String str) {
+    return str.toUpperCase();
+  }
+
+  textToLower(String str) {
+    return str.toLowerCase();
+  }
+
+  textReversed(String str) {
+    return str.split('').reversed.join();
+  }
+
+  convertText(String str) {
+    switch (_dropdownValue) {
+      case 'Reverso':
+        return textReversed(str);
+        break;
+      case 'MAÍUSCULAS':
+        return textToUpper(str);
+        break;
+      case 'minúsculas':
+        return textToLower(str);
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                maxLines: 5,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  filled: true,
+                  hintText: 'Insira o texto aqui...',
+                  labelText: 'Seu Texto',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Por favor insira um texto.';
+                  }
+                  return null;
+                },
+                onChanged: (text) {
+                  this._userText = text;
+                  print("First text field: $text");
+                },
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 125,
+                      child: DropdownButtonFormField<String>(
+                        decoration: InputDecoration.collapsed(hintText: ''),
+                        validator: (value) =>
+                            value == 'Selecione' ? 'Selecione um valor.' : null,
+                        value: _dropdownValue,
+                        onChanged: (String newValue) {
+                          setState(() {
+                            _dropdownValue = newValue;
+                          });
+                        },
+                        items: opcoes
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                    Container(
+                      child: RaisedButton(
+                        onPressed: () {
+                          if (_formKey.currentState.validate()) {
+                            setState(() {
+                              _myController.text = convertText(_userText);
+                            });
+                          }
+                        },
+                        child: const Text(
+                          'Converter',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        TextFormField(
+          maxLines: 5,
+          controller: _myController,
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            filled: true,
+            labelText: 'Resultado',
+          ),
+        ),
+      ],
+    );
+  }
+}
